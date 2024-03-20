c72cc615239f0ad29166731435b8b09e0cd76861
mattteo-st <matteo.sammut@hotmail.fr>
2024-03-20 15:13:52 +0000

    vm_setup OK

--- vm_setup/setup_minkowski.sh
+++ vm_setup/setup_minkowski.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run this script from the project root directory
-cd ~4D-convnet 
+cd ~/4D-convnet
 
 # Create and activate conda environment
 conda create -n py3-mink python=3.8 -y
@@ -9,9 +9,9 @@ source activate py3-mink
 
 # Install dependencies
 conda install openblas-devel -c anaconda -y
-conda install pytorch=1.7.1 torchvision cudatoolkit=11.8 -c pytorch -c conda-forge -y
+conda install pytorch torchvision cudatoolkit=11.8 -c pytorch -c conda-forge -y
 
 cd code
 git clone https://github.com/NVIDIA/MinkowskiEngine.git
 cd MinkowskiEngine
-python setup.py install --blas_include_dirs=${CONDA_PREFIX}/include --blas=openblas
+python setup.py install --blas_include_dirs=${CONDA_PREFIX}/include --blas=openblas --force_cuda
