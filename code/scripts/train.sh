aebf08b8fe093967696cc39760b79e029f864df3
mattteo-st <matteo.sammut@hotmail.fr>
2024-03-24 07:19:33 +0000

    remove code folder

--- code/scripts/train.sh
+++
@@ -1,8 +0,0 @@
-cd ~/4D-convnet
-
-CUDA_VISIBLE_DEVICES=0                                                      \
-python                                                                      \
-code/main.py                                                                 \
---batch_size             4                                                  \
---n_smaple 10 \
---data_dir .data \
